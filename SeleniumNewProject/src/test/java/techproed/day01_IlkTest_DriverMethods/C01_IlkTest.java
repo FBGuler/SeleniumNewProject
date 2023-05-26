fc0b44e572c04ecbb7d54ca4f3489cbc73f15119
fbguler <banuguler2016@gmail.com>
2023-05-26 22:36:42 +0100

    Initial commit

---
+++ SeleniumNewProject/src/test/java/techproed/day01_IlkTest_DriverMethods/C01_IlkTest.java
@@ -0,0 +1,18 @@
+package techproed.day01_IlkTest;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkTest {
+    public static void main(String[] args) {
+
+        System.setProperty("chromeDriver","src/resources/drivers/chromedriver.exe");
+        //Java uygulamalarinda system ozelliklerini ayarlamak icin setProperty methodu ile kullaniriz.
+        System.out.println(System.getProperty("chromeDriver"));
+        //getProperty ile "Key" degerini girerek "value" ya ulasabilirim.
+        WebDriver driver = new ChromeDriver();
+        //ChromeDriver turunde yeni bir obje Olusturduk
+        driver.get("https://techproeducation.com");
+    }
+}
