ce8b96b6e84abd937ebdf1077e59468c3ca7e18c
fbguler <banuguler2016@gmail.com>
2023-05-30 13:08:27 +0100

    27/05/23/

---
+++ SeleniumNewProject/src/test/java/techproed/day03_Locators/C01_WebElementLocators.java
@@ -0,0 +1,50 @@
+package techproed.day03_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_WebElementLocators {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        // https://www.amazon.com sayfasına gidiniz
+        // aramakutusunu locate ediniz ve Nutella aratınız
+        // sayfayı kapatınız
+
+        driver.get("https://www.amazon.com");
+        //WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nutella" +Keys.ENTER);
+        Thread.sleep(2000);
+        driver.close();
+
+        /*
+        Eger bir webElementi birden fazla kullanmayacaksaniz bir webElemente atama yapmaya gerek yok.
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
